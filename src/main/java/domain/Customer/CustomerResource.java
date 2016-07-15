0772e293458343c280f126e224a2b1c486877eca
Sharipov <pavel.sharipov@gmail.com>
2016-07-15 15:37:54 +0300

    second

--- src/main/java/domain/Customer/CustomerResource.java
+++ src/main/java/domain/Customer/CustomerResource.java
@@ -1,19 +1,30 @@
 package domain.Customer;
 
 import java.io.InputStream;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.List;
 
+
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/customers")
 public class CustomerResource {
-	private Map<Integer, Customer> customerDB = new ConcurrentHashMap<Integer, Customer>();
-	private AtomicInteger idCounter = new AtomicInteger();
+	
+	
+	@Inject
+	private CustomerRepository customRepository;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Customer> findAll() {
+		return customRepository.getAll();
+	}
 	
 	@POST
 	@Consumes("application/xml")
