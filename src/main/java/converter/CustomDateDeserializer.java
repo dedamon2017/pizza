d86b37bf8158c306abfd99ab1ac30701607bb79e
dedamon2016 <pavel.sharipov@gmail.com>
2016-08-04 23:52:12 +0300

    converter/CustomDateDeserializer.java converter/CustomDateSerializer.java

---
+++ src/main/java/converter/CustomDateDeserializer.java
@@ -0,0 +1,26 @@
+package converter;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+public class CustomDateDeserializer extends JsonDeserializer<Date>{
+	private static SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	@Override
+	public Date deserialize(JsonParser  jsonparser, DeserializationContext  context) throws IOException, JsonProcessingException {
+        String date = jsonparser.getText();
+        try	{
+        	return formatter.parse(date);
+        } 
+        catch (ParseException exception) {
+        	throw new RuntimeException(exception);
+        }
+	}
+
+}
